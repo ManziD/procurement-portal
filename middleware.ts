2700f5ab3ad1893f1ab136c0e1dba30d5ade1aa8
ManziD <117282426+ManziD@users.noreply.github.com>
2026-08-21 18:35:47 +0300

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -38,6 +38,6 @@ export async function middleware(request: NextRequest) {
 
 export const config = {
   matcher: [
-    '/((?!_next/static|_next/image|favicon.ico|public|dashboard).*)',
-  ],
+  '/((?!_next/static|_next/image|favicon.ico|public|dashboard|dashboard-new).*)',
+],
 }
